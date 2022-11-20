b6979e1d3a428492ebe8112c1c6b02c46dda51a6
Alec Brown <12213854+a1ecbr0wn@users.noreply.github.com>
2022-11-20 22:53:28 +0000

    Analytics tracking (#35)

---
+++ src/init/bash.rs
@@ -0,0 +1,73 @@
+use home::home_dir;
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+
+const BASH_INIT: &str = "eval \"$(bookmark-cd init)\"";
+
+pub(crate) fn instructions_bash() {
+    println!("To complete setup, please edit your ~/.bashrc file and insert the following to the end of the file:");
+    println!("# bookmark-cd init block");
+    println!("{}", BASH_INIT);
+}
+
+pub(crate) fn check_bash() -> bool {
+    let mut bashrc_file = home_dir().unwrap();
+    bashrc_file.push(".bashrc");
+    if bashrc_file.exists() {
+        let file_res = File::open(bashrc_file);
+        if let Ok(mut file) = file_res {
+            let mut contents = String::new();
+            match file.read_to_string(&mut contents) {
+                Ok(_) => contents.contains(BASH_INIT),
+                Err(_) => {
+                    println!("Cannot read `~/.bashrc` to install bookmark-cd (bcd) to run in your shell [bash]");
+                    false
+                }
+            }
+        } else {
+            println!(
+                "Cannot open `~/.bashrc` to install bookmark-cd (bcd) to run in your shell [bash]"
+            );
+            false
+        }
+    } else {
+        println!(
+            "Cannot find `~/.bashrc` to install bookmark-cd (bcd) to run in your shell [bash]"
+        );
+        false
+    }
+}
+
+pub(crate) fn setup_bash(interactive: bool) {
+    let mut bashrc_file = home_dir().unwrap();
+    bashrc_file.push(".bashrc");
+    if bashrc_file.exists() {
+        let res = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(bashrc_file);
+        match res {
+            Ok(mut file) => {
+                writeln!(file).unwrap();
+                writeln!(file, "# bookmark-cd init block").unwrap();
+                writeln!(file, "{}", BASH_INIT).unwrap();
+                writeln!(file).unwrap();
+                if interactive {
+                    println!(
+                        "Your shell init script has been set up, restart your shell and type `bcd`"
+                    );
+                }
+            }
+            Err(_) => {
+                if interactive {
+                    println!("Unable to open your shell init script.");
+                }
+            }
+        }
+    } else if interactive {
+        println!(
+            "Shell init script [{}] not found",
+            bashrc_file.to_str().unwrap()
+        );
+    }
+}
