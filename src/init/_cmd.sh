925d297b5273f2a961234df8fa6e37e937595c91
a1ecbr0wn <alec@noser.net>
2022-12-20 15:41:14 +0000

    zsh compatability, '==' is not posix standard when testing string equality, using '=' instead

--- src/init/_cmd.sh
+++ src/init/_cmd.sh
@@ -1,8 +1,8 @@
 function bcd() {
     result=$(bookmark-cd $@ 2>&1)
-    if [ "cd" == ${result:0:2} ]; then
+    if [ "cd" = ${result:0:2} ]; then
         cd ${result:3:${#result}-3}
     else
         echo "$result"
     fi
-}
+ }
